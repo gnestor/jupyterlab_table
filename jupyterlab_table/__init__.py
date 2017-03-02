fb9351962d3ae9971b11ec635fe3951c90df9485
Grant Nestor <grantnestor@gmail.com>
2017-03-01 17:07:41 -0800

    Support DataFrames in display class

--- jupyterlab_table/__init__.py
+++ jupyterlab_table/__init__.py
@@ -1,5 +1,7 @@
-from IPython.display import display, JSON
+from IPython.display import display, DisplayObject
 import json
+import pandas as pd
+from .utils import prepare_data
 
 
 # Running `npm run build` will create static resources in the static
@@ -23,22 +25,77 @@ def _jupyter_nbextension_paths():
 
 # A display class that can be used within a notebook. E.g.:
 #   from jupyterlab_table import JSONTable
-#   JSONTable(data)
+#   JSONTable(data, schema)
     
-class JSONTable(JSON):
+class JSONTable(DisplayObject):
     """A display class for displaying JSONTable visualizations in the Jupyter Notebook and IPython kernel.
     
-    JSONTable expects a JSON-able dict, not serialized JSON strings.
+    JSONTable expects a JSON-able list, not serialized JSON strings.
 
     Scalar types (None, number, string) are not allowed, only dict containers.
     """
+    # wrap data in a property, which warns about passing already-serialized JSON
+    _data = None
+    _schema = None
+    def __init__(self, data=None, schema=None, url=None, filename=None, metadata=None):
+        """Create a JSON Table display object given raw data.
 
-    def _data_and_metadata(self):
-        return self.data, self.metadata
-    
+        Parameters
+        ----------
+        data : list
+            Not an already-serialized JSON string.
+            Scalar types (None, number, string) are not allowed, only list containers.
+        schema : dict
+            JSON Table Schema. See http://frictionlessdata.io/guides/json-table-schema/.
+        url : unicode
+            A URL to download the data from.
+        filename : unicode
+            Path to a local file to load the data from.
+        metadata: dict
+            Specify extra metadata to attach to the json display object.
+        """
+        self.schema = schema
+        self.metadata = metadata
+        super(JSONTable, self).__init__(data=data, url=url, filename=filename)
+
+    def _check_data(self):
+        if self.data is not None and not isinstance(self.data, (list, pd.DataFrame)):
+            raise TypeError("%s expects a JSONable list or pandas DataFrame, not %r" % (self.__class__.__name__, self.data))
+        if self.schema is not None and not isinstance(self.schema, dict):
+            raise TypeError("%s expects a JSONable dict, not %r" % (self.__class__.__name__, self.schema))
+        
+    @property
+    def data(self):
+        return self._data
+        
+    @property
+    def schema(self):
+        return self._schema
+
+    @data.setter
+    def data(self, data):
+        if isinstance(data, str):
+            # warnings.warn("JSONTable expects JSON-able dict or list, not JSON strings")
+            data = json.loads(data)
+        self._data = data
+        
+    @schema.setter
+    def schema(self, schema):
+        if isinstance(schema, str):
+            # warnings.warn("JSONTable expects a JSON-able list, not JSON strings")
+            schema = json.loads(schema)
+        self._schema = schema
+        
     def _ipython_display_(self):
         bundle = {
-            'application/vnd.dataresource+json': self.data,
+            'application/vnd.dataresource+json': {
+                'resources': [
+                    {
+                        'schema': self.schema,
+                        'data': prepare_data(self.data)
+                    }
+                ]
+            },
             'text/plain': '<jupyterlab_table.JSONTable object>'
         }
         metadata = {
